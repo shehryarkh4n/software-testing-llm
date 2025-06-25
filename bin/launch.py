bbd710a526e42f504d07e288b655f4b9e0385252
Muhammad Shehryar Khan <shehryarkhan@tc-dgx005.cm.cluster>
2025-06-25 15:53:15 -0400

    fix launch; add README

--- bin/launch.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from src.training.trainer import train_main
-
-if __name__ == "__main__":
-    # Expects: torchrun bin/launch.py path/to/config.yaml
-    config_path = sys.argv[1]
-    train_main(config_path=config_path)
